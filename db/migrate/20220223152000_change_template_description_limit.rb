fd6e2ef9dab692a1ac79374e999de449bcd32ec6
Björn Friebel <bjoern.friebel@aviotech.de>
2022-02-23 16:54:46 +0100

    adapt (global) issue template description size limit to redmine limits

---
+++ db/migrate/20220223152000_change_template_description_limit.rb
@@ -0,0 +1,13 @@
+class ChangeTemplateDescriptionLimit < ActiveRecord::Migration[5.2]
+  def up
+    if Redmine::Database.mysql?
+      max_size = 16.megabytes
+      change_column :issue_templates, :description, :text, :limit => max_size
+      change_column :global_issue_templates, :description, :text, :limit => max_size
+    end
+  end
+
+  def down
+    # no-op
+  end
+end
